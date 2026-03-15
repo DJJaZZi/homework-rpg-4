d56ab3297253281e54d90a6e60c28474bffac013
DJJaZZi <siganak18@gmail.com>
2026-03-15 21:22:44 +0500

    EnemyUnit changed

--- src/com/narxoz/rpg/composite/EnemyUnit.java
+++ src/com/narxoz/rpg/composite/EnemyUnit.java
@@ -1,7 +1,7 @@
 package com.narxoz.rpg.composite;
 
 public class EnemyUnit extends UnitLeaf {
-    public EnemyUnit(String name, int health, int attackPower) {
-        super(name, health, attackPower);
+    public EnemyUnit(String name, int power, int health, int attackPower) {
+        super(name, power, health, attackPower);
     }
 }
